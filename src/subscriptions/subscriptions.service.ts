b3bbdb424db87c7cbe39f79a9db9dbdce5b9799d
Arya Sh <shahali.arya@gmail.com>
2024-11-26 22:22:41 +0100

    update

--- src/subscriptions/subscriptions.service.ts
+++ src/subscriptions/subscriptions.service.ts
@@ -1,6 +1,4 @@
 import { Injectable } from '@nestjs/common';
-import { CreateSubscriptionDto } from './dto/create-subscription.dto';
-import { UpdateSubscriptionDto } from './dto/update-subscription.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Subscription } from './entities/subscription.entity';
