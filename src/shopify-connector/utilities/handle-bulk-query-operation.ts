40fa47922e6455fa83dd3868fd8bf164e1b1ca8d
Arya Sh <shahali.arya@gmail.com>
2026-05-25 11:09:10 +0200

    release 0.0.338

--- src/shopify-connector/utilities/handle-bulk-query-operation.ts
+++ src/shopify-connector/utilities/handle-bulk-query-operation.ts
@@ -1,17 +1,20 @@
-import { ShopifyConfig } from "../shopify-connector.service";
-import { fetchBulkOperationResults } from "./fetch-bulk-operation-results";
-import { pollBulkOperationStatus } from "./poll-bulk-operation-status";
-import { runBulkQuery } from "./run-bulk-query";
+import { ShopifyConfig } from '../shopify-connector.service';
+import { fetchBulkOperationResults } from './fetch-bulk-operation-results';
+import { pollBulkOperationStatus } from './poll-bulk-operation-status';
+import { runBulkQuery } from './run-bulk-query';
 
-export const handleBulkQueryOperation = async (shopifyConfig: ShopifyConfig, query: string) =>  {
-    try {
-      const operationId = await runBulkQuery(shopifyConfig, query);
-      const resultUrl = await pollBulkOperationStatus(shopifyConfig, operationId);
-      const results = await fetchBulkOperationResults(resultUrl);
+export const handleBulkQueryOperation = async (
+  shopifyConfig: ShopifyConfig,
+  query: string,
+) => {
+  try {
+    const operationId = await runBulkQuery(shopifyConfig, query);
+    const resultUrl = await pollBulkOperationStatus(shopifyConfig, operationId);
+    const results = await fetchBulkOperationResults(resultUrl);
 
-      return results;
-    } catch (error) {
-      console.error('Error fetching processing bulk operation:', error);
-      throw error;
-    }
+    return results;
+  } catch (error) {
+    console.error('Error fetching processing bulk operation:', error);
+    throw error;
   }
+};
